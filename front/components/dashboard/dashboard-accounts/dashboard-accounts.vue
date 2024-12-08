4a262b17506f3e7772855bb485b89cd4751188ea
Blas Gómez <blasgomossa@gmail.com>
2024-12-08 22:54:00 +0000

    Update Accounts dashboard

--- front/components/dashboard/dashboard-accounts/dashboard-accounts.vue
+++ front/components/dashboard/dashboard-accounts/dashboard-accounts.vue
@@ -1,9 +1,9 @@
 <template>
   <van-cell-group inset style="overflow: auto">
-    <div class="flex-center-vertical">
-      <div class="van-cell-group-title">Total balance:</div>
-      <div class="flex-1" />
-
+    <div class="flex-center-vertical justify-between">
+      <!-- <div class="van-cell-group-title">Total balance:</div> -->
+      <!-- <div class="flex-1" /> -->
+      
       <van-button v-if="hasMultipleCurrencies" @click="onToggleTotalCurrency" size="small" class="mr-10">
         <template #icon>
           <app-icon :icon="TablerIconConstants.transaction" :size="20" />
@@ -11,23 +11,33 @@
         </template>
       </van-button>
 
+      <div class="flex-center text-size-13 m-10 flex-wrap justify-center flex-grow-1">
+        <div class="flex-center text-size-13 me-1">
+          <icon-cash class="text-muted" :size="50" :stroke="2" />
+          <span class="font-700 text-size-26">Total: </span>
+        </div>
+
+        <span v-for="(totalValue, totalCurrency) in dataStore.dashboardAccountsTotalByCurrency" class="font-700 text-size-26 ms-1 mx-1 app-select-option-tag">
+          {{ getFormattedValue(totalValue) }} {{ totalCurrency }}
+        </span>
+      </div>
+
       <van-button @click="onToggleShowDashboardAccountValues" size="small" class="mr-10">
         <template #icon>
           <app-icon :icon="profileStore.dashboard.showAccountAmounts ? TablerIconConstants.eyeHidden : TablerIconConstants.eyeVisible" :size="20" />
         </template>
       </van-button>
     </div>
-
-    <van-grid :column-num="2">
-      <van-grid-item v-for="account in visibleDashboardAccounts" :key="account.id" @click="onGoToTransactions(account)" icon="photo-o">
-        <template #icon>
-          <app-icon :icon="Account.getIcon(account) ?? TablerIconConstants.account" :size="24" />
-        </template>
-
+    
+    <van-grid :column-num="4">
+      <van-grid-item v-for="account in visibleDashboardAccounts" :key="account.id" @click="onGoToTransactions(account)" class="left-align-grid-item">
         <template #text>
-          <div class="display-flex flex-column align-items-center">
-            <div class="font-400 text-size-12 text-center text-muted">{{ Account.getDisplayName(account) }}</div>
-            <div class="font-700 text-size-12 text-center">{{ getAccountAmount(account) }}</div>
+          <div class="display-flex align-items-left">
+            <app-icon :icon="Account.getIcon(account) ?? TablerIconConstants.account" :size="50" class="me-2" />
+            <div>
+              <div class="font-400 text-size-16 text-muted">{{ Account.getDisplayName(account) }}</div>
+              <div :class="getAmountClass(extractNumericValue(getAccountAmount(account)))" class="font-700 text-size-16">{{ getAccountAmount(account) }}</div>
+            </div>
           </div>
         </template>
       </van-grid-item>
@@ -37,17 +47,6 @@
       <div @click="toggleHiddenAccounts" class="p-5 m-5 button-link">
         {{ showHiddenAccounts ? 'View less...' : 'View more...' }}
       </div>
-    </div>
-
-    <div class="flex-center text-size-13 m-10 flex-wrap">
-      <div class="flex-center text-size-13 me-1">
-        <icon-cash class="text-muted" :size="24" :stroke="1.5" />
-        <span class="font-400 text-muted">Total: </span>
-      </div>
-
-      <span v-for="(totalValue, totalCurrency) in dataStore.dashboardAccountsTotalByCurrency" class="font-700 ms-1 mx-1 app-select-option-tag">
-        {{ getFormattedValue(totalValue) }} {{ totalCurrency }}
-      </span>
     </div>
 
     <div v-if="hasMultipleCurrencies" class="flex-center text-size-13 mb-3 gap-1">
@@ -86,9 +85,23 @@ const onToggleShowDashboardAccountValues = async () => {
   profileStore.dashboard.showAccountAmounts = !profileStore.dashboard.showAccountAmounts
 }
 
+const extractNumericValue = (value) => {
+  return parseFloat(value.replace(/[^0-9.-]+/g, ''))
+}
+
 const getAccountAmount = (account) => {
   return `${getFormattedValue(Account.getBalance(account))} ${Account.getCurrency(account)}`
 }
+
+const getAmountClass = (amount) => {
+      if (amount > 0) {
+        return 'amount-positive';
+      } else if (amount < 0) {
+        return 'amount-negative';
+      } else {
+        return 'amount-zero';
+      }
+    }
 
 const hasMultipleCurrencies = computed(() => dataStore.dashboardAccountsCurrencyList.length > 1)
 
